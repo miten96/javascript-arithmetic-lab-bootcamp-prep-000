290b420698f67099c8417420fbc9c7a7e90ea9c6
Miten Kashiyani <miten.kashiyani96@gmail.com>
2020-05-24 13:12:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ function add(a,b) {
   return a+b
 }
 
+function substract(a,b) {
+  return a-b
+}
+
+function multiple(a,b) {
+  return a%b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+
+
 function makeInt(string) {
   parseInt('string', 10)
 }
